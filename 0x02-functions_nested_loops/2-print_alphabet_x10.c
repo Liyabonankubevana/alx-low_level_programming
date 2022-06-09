5fe77c65153fa4c570dd796c49a21b8632cfdea7
Liyabona <liyabonankubevana94@gmail.com>
2022-06-09 02:21:05 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 include "main.h"
 
 /**
- * print_alphabet_x10 -> Prints the alphabet 10 times
+ * print_alphabet_x10 - Prints the alphabet 10 times
  */
 	
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 	int = j;
 	int = x;
